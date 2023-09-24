eac37a11e6ea58059c17b663711de3fb9a67e96c
KrzysztofPak <117757357+KrzysztofPak@users.noreply.github.com>
2023-09-24 13:25:15 +0200

    commit

---
+++ middlewares/routes/api/contacts.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const { getAll, getById, add, removeById, updateById, updateByFavorite } = require('../../controllers/contacts');
+const { validateBody, isValidId } = require('../../middlewares');
+const { bodySchema, updateByFavoriteSchema } = require('../../schemas/contacts');
+
+const router = express.Router();
+
+router.get("/", getAll);
+router.get("/:contactId", isValidId, getById);
+router.post("/", validateBody(bodySchema),add);
+router.delete("/:contactId", isValidId, removeById);
+router.put("/:contactId", isValidId, validateBody(bodySchema), updateById);
+router.patch("/:contactId/favorite", isValidId, validateBody(updateByFavoriteSchema),updateByFavorite);
+
+module.exports = router;
